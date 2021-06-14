a1c17dbc8f7b015a51fd8268e7edd42a4f5a9b80
imbruh <brunaluiza112001@gmail.com>
2021-06-14 19:50:41 -0300

    adicionando a logica no front

---
+++ ediaristas/src/ui/components/surfaces/Footer/Footer.tsx
@@ -0,0 +1,40 @@
+import { Typography, Box } from '@material-ui/core';
+import React from 'react';
+import { FooterStyled, FooterContainer, FooterTitle, AppList } from './Footer.style';
+
+const Footer = () => {
+    return (
+        <FooterStyled>
+            <FooterContainer>
+                <Box sx={{maxWidth: '400px'}}>
+                    <FooterTitle>Quem somos?</FooterTitle>
+                    <Typography variant={'body2'} sx={{marginTop: 2}}>
+                        E-Diaristas te ajuda a encontrar um profissional perfeito
+                        para realizar a limpeza da sua casa. Garantimos a melhor profissional
+                        com total segurança e praticidade! São milhares de clientes satisfeitos por todo o país.
+                    </Typography>
+                </Box>
+                
+                <div>
+                    <FooterTitle>Baixe nossos aplicativos</FooterTitle>
+                    <AppList>
+                        <li>
+                            <a href={'/'} target={'_blank'} rel={'noopener noreferrer'}>
+                                <img src="/img/logos/app-store.png" alt="" />
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href={'/'} target={'_blank'} rel={'noopener noreferrer'}>
+                                <img src="/img/logos/google-play.png" alt="" />
+                            </a>
+                        </li>
+                    </AppList>
+                </div>
+
+            </FooterContainer>
+        </FooterStyled>
+    )
+}
+
+export default Footer;
